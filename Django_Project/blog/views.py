cf714a99e5b8dca2d96de6ebb8edd7b74726615c
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-05-19 20:15:53 +0530

    change database and other changes

--- Django_Project/blog/views.py
+++ Django_Project/blog/views.py
@@ -1,23 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
-posts = [
-		{
-			'author':"Jasbir",
-			'title':"Blog Post 1",
-			'content':"First post content",
-			'date_posted':"May 18, 2020"
-		},
-		{
-			'author':"Shoaib",
-			'title':"Blog Post 2",
-			'content':"Second post content",
-			'date_posted':"May 19, 2020"
-		},
-]
 def home(request):
 	context = {
-		'posts':posts
+		'posts':Post.objects.all()
 	}
 	return render(request,'blog/home.html',context)
 
